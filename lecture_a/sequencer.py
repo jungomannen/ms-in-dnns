7515818b9692a495dc0c5682d07f96f088e8c57c
jungomannen <156915822+jungomannen@users.noreply.github.com>
2024-01-18 13:27:08 +0100

    Changed venv and reformatted sequencer.py

--- lecture_a/sequencer.py
+++ lecture_a/sequencer.py
@@ -1,19 +1,20 @@
 import argparse
 import sys
 
+
 def main(args):
     arg_type = args.sequence
     numbers = args.length
     if arg_type == "triangular":
-        return [n*(n+1)//2 for n in range(1, numbers+1)]
+        return [n * (n + 1) // 2 for n in range(1, numbers + 1)]
     if arg_type == "square":
-        return [n*n for n in range(1, numbers+1)]
+        return [n * n for n in range(1, numbers + 1)]
     if arg_type == "factorial":
         res = [1]
         if numbers <= 1:
             return res
-        for n in range(2, numbers+1):
-            res.append(res[-1]*n)
+        for n in range(2, numbers + 1):
+            res.append(res[-1] * n)
         return res
     if arg_type == "fibonacci":
         res = [0, 1]
@@ -22,13 +23,14 @@ def main(args):
         if numbers == 2:
             return res
         for n in range(2, numbers):
-            res.append(res[-1]+res[-2])
+            res.append(res[-1] + res[-2])
         return res
+
     if arg_type == "prime":
         res = [2]
         if numbers <= 1:
             return res
-        i=3
+        i = 3
         while len(res) < numbers:
             is_prime = True
             for prime in res:
@@ -40,14 +42,11 @@ def main(args):
             i += 1
         return res
 
-    
-
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--length', dest='length', type=int)
-    parser.add_argument('--sequence', dest = 'sequence')
+    parser.add_argument("--length", dest="length", type=int)
+    parser.add_argument("--sequence", dest="sequence")
     try:
         result = main(parser.parse_args())
         if result == None:
@@ -55,4 +54,4 @@ if __name__ == "__main__":
         else:
             print(result)
     except:
-        print('invalid choice', file=sys.stderr)
+        print("invalid choice", file=sys.stderr)
