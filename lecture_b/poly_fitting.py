ad0542c36cd7e4693acc01cf3c03c32d05a7656d
jungomannen <156915822+jungomannen@users.noreply.github.com>
2024-01-24 11:52:00 +0100

    Created case k=1 for linear regression plotting

--- lecture_b/poly_fitting.py
+++ lecture_b/poly_fitting.py
@@ -25,18 +25,26 @@ def mse_poly(x, y, W):
 
 
 def create_polynomial(W):
-    return lambda x: sum(w * pow(x, n) for n, w in enumerate(W))
+    return lambda x: sum(w * pow(x, n) for n, w in enumerate(reversed(W)))
 
 
 def main():
     # generate training and test data
-    x_training_data, y_training_data = generate_data(15)
+    x_training_data, y_training_data = generate_data(100)
     x_test_data, y_test_data = generate_data(10)
+
+    W = fit_poly(x_training_data, y_training_data)
+    print(W)
+
+    x_fitted_data = np.linspace(0, 2 * np.pi, 30)
+    fitted_polynomial = create_polynomial(W)
+    y_fitted_data = fitted_polynomial(x_fitted_data)
 
     # plot
     fig, ax = plt.subplots()
 
     ax.plot(x_training_data, y_training_data)
+    ax.plot(x_fitted_data, y_fitted_data)
 
     # ax.set(xlim=(0, 2 * np.pi), ylim=(1.5, 1.5))
 
